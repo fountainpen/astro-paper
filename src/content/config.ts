900bd6ed7c33b494e0f9b9294bcc624c4a78d31e
fountainpen <parsifal02@msn.com>
2023-11-02 07:12:14 +0100

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const blog = defineCollection({
   schema: ({ image }) =>
     z.object({
       author: z.string().default(SITE.author),
-      pubDatetime: z.date(),
+      pubDate: z.date(),
       title: z.string(),
       postSlug: z.string().optional(),
       featured: z.boolean().optional(),
